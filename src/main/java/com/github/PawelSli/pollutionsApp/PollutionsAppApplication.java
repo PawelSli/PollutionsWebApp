56bde557f2d0625f84a4696c73ec959eae86e65a
PawelSli <ps439787@gmail.com>
2021-02-18 21:59:15 +0100

    Initial commit

---
+++ src/main/java/com/github/PawelSli/pollutionsApp/PollutionsAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.PawelSli.pollutionsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollutionsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollutionsAppApplication.class, args);
+	}
+
+}
