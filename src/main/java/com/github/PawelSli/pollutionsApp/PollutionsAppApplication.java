0f64a9625b2f7d0a7350fb0c07ea9c7b9f2609f0
PawelSli <ps439787@gmail.com>
2021-02-18 23:51:51 +0100

    Initial configuration of Spring Boot

--- src/main/java/com/github/PawelSli/pollutionsApp/PollutionsAppApplication.java
+++ src/main/java/com/github/PawelSli/pollutionsApp/PollutionsAppApplication.java
@@ -1,13 +1,26 @@
 package com.github.PawelSli.pollutionsApp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PollutionsAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollutionsAppApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext applicationContext){
+		return args -> {
+			System.out.println("Let's inspect beans provided by Spring Boot");
+			String[] beansNames=applicationContext.getBeanDefinitionNames();
+			for(String beanName:beansNames)
+				System.out.println(beanName);
+		};
 	}
 
 }
