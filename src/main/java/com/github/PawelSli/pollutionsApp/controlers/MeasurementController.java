7f2baf6b0c5152aa4d95f00fb0d4c484c10f9bdb
PawelSli <ps439787@gmail.com>
2021-02-22 23:51:45 +0100

    Getting all cities from URL to list in HTML view

--- src/main/java/com/github/PawelSli/pollutionsApp/controlers/MeasurementController.java
+++ src/main/java/com/github/PawelSli/pollutionsApp/controlers/MeasurementController.java
@@ -3,15 +3,23 @@ package com.github.PawelSli.pollutionsApp.controlers;
 import com.github.PawelSli.pollutionsApp.repository.MeasurementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MeasurementController {
+    private MeasurementRepository measurementRepository;
+
+    public MeasurementController(){
+        this.measurementRepository=new MeasurementRepository();
+    }
 
     @RequestMapping("/main")
-    public String showMainPanel(){
+    public String showMainPanel(Model model){
+        model.addAttribute("cities",measurementRepository.getJsonArrayFromUrl());
         return "main";
     }
 
--- src/main/java/com/github/PawelSli/pollutionsApp/repository/MeasurementRepository.java
+++ src/main/java/com/github/PawelSli/pollutionsApp/repository/MeasurementRepository.java
@@ -2,37 +2,32 @@ package com.github.PawelSli.pollutionsApp.repository;
 
 
 import netscape.javascript.JSObject;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class MeasurementRepository {
 
-    public void getData(){
-
-    }
-
-    private static String readAll(Reader reader) throws IOException {
-        StringBuilder stringBuilder=new StringBuilder();
-        int i=0;
-        while ((i = reader.read()) != -1) {
-            stringBuilder.append((char) i);
-        }
-        return stringBuilder.toString();
-    }
-
-    private static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        InputStream is = new URL(url).openStream();
+    public  ArrayList getJsonArrayFromUrl(){
+        ArrayList<String> arrayList=new ArrayList<>();
         try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            JSONObject json = new JSONObject(jsonText);
-            return json;
-        } finally {
-            is.close();
+            String text = new Scanner(new URL("http://api.gios.gov.pl/pjp-api/rest/station/findAll").openStream()).useDelimiter("\\A").next();
+            JSONArray array = new JSONArray(text);
+            array.forEach(object->{
+                JSONObject jsonObject=(JSONObject) object;
+                arrayList.add((String)jsonObject.get("stationName"));
+            });
+            Collections.sort(arrayList);
+        }catch (IOException exception){
+            exception.printStackTrace();
         }
+        return arrayList;
     }
 }
